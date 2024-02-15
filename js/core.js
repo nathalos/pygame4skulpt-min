faa36415885a07ffce2e58a127a9f8a5ec3d30af
Nathanael Ng <nathanael.h.e.ng@gmail.com>
2024-02-15 16:20:05 +0800

    Updated with cookie module and misc sample changes.

--- js/core.js
+++ js/core.js
@@ -1,63 +1,72 @@
 "use strict";
 const basePath = 'https://cdn.rawgit.com/Petlja/pygame4skulpt/3435847b/pygame/';
 Sk.externalLibraries = {
-	'pygame': 			{ path: basePath + '__init__.js' },
-	'pygame.display':   { path: basePath + 'display.js' },
-	'pygame.draw': 		{ path: basePath + 'draw.js' },
-	'pygame.event': 	{ path: basePath + 'event.js' },
-	'pygame.font': 		{ path: basePath + 'font.js' },
-	'pygame.image': 	{ path: basePath + 'image.js' },
-	'pygame.key': 		{ path: basePath + 'key.js' },
-	'pygame.mouse': 	{ path: basePath + 'mouse.js' },
-	'pygame.time': 		{ path: basePath + 'time.js' },
-	'pygame.transform': { path: basePath + 'transform.js' },
-	'pygame.version': 	{ path: basePath + 'version.js' },
-	//'skulptcookies':   { path: "./lib/skulptcookies.js" } //Uses AJAX. I would need to publish SkulptCookies on a CDN to get it working.
+    'pygame': { path: basePath + '__init__.js' },
+    'pygame.display': { path: basePath + 'display.js' },
+    'pygame.draw': { path: basePath + 'draw.js' },
+    'pygame.event': { path: basePath + 'event.js' },
+    'pygame.font': { path: basePath + 'font.js' },
+    'pygame.image': { path: basePath + 'image.js' },
+    'pygame.key': { path: basePath + 'key.js' },
+    'pygame.mouse': { path: basePath + 'mouse.js' },
+    'pygame.time': { path: basePath + 'time.js' },
+    'pygame.transform': { path: basePath + 'transform.js' },
+    'pygame.version': { path: basePath + 'version.js' },
+    'skulptcookies':   { path: "./lib/skulptcookies.js" } //Uses AJAX. I would need to publish SkulptCookies on a CDN to get it working.
 };
 
 const app = {
-	init: function() {
-		(Sk.TurtleGraphics || (Sk.TurtleGraphics = {})).target = 'mycanvas';
-		Sk.configure({read: app.builtinRead, output: console.log});
-		const canvas = $("canvas");
-		app.baseCanvasSize = { width: canvas.width(), height:canvas.height() , aspect: canvas.width()/canvas.height()};
-		$(window).on("resize",app.fitToScreen);
-		app.fitToScreen();
-		app.runCode();
-	},
-	
-	fitToScreen: function() {
-		const canvas = $("canvas");
-		if (innerWidth/innerHeight < app.baseCanvasSize.aspect)  //too narrow, squash vertically
-			canvas.width(innerWidth).height(innerWidth/app.baseCanvasSize.aspect);
-		else
-			canvas.height(innerHeight).width(innerHeight*app.baseCanvasSize.aspect);
-	},
+    init: function() {
+        (Sk.TurtleGraphics || (Sk.TurtleGraphics = {})).target = 'mycanvas';
+        Sk.configure({ read: app.builtinRead, output: console.log });
+        const canvas = $("canvas");
+        //bah CSS loading bug makes this break on replit
+        //        app.baseCanvasSize = { width: canvas.width(), height: canvas.height(), aspect: canvas.width() / canvas.height() };
+        app.baseCanvasSize = { width: 1280, height: 720, aspect: 16 / 9 };
+        $(window).on("resize", app.fitToScreen);
+        app.fitToScreen();
+        app.runCode();
+    },
 
-	builtinRead: function(x) {
-		if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined)
-			throw "File not found: '" + x + "'";
-		return Sk.builtinFiles["files"][x];
-	},
-	
-	loadScript: function(path) {
-		$.get(path, function(data) { $("#program").text(data) });
-	},
+    fitToScreen: function() {
+        const canvas = $("canvas");
+        console.log(canvas.width(), canvas.height(), innerWidth, innerHeight);
+        if (innerWidth / innerHeight < app.baseCanvasSize.aspect)  //too narrow, squash vertically
+            canvas.width(innerWidth).height(innerWidth / app.baseCanvasSize.aspect);
+        else
+            canvas.height(innerHeight).width(innerHeight * app.baseCanvasSize.aspect);
+    },
 
-	runCode: function() {
-		Sk.main_canvas = $("canvas")[0];
-		
-		//WIP: load an external script using AJAX
-		const prog = $("#program").text();
+    builtinRead: function(x) {
+        if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined)
+            throw "File not found: '" + x + "'";
+        return Sk.builtinFiles["files"][x];
+    },
 
-		Sk.misceval.asyncToPromise(function () {
-			try {
-				return Sk.importMainWithBody("<stdin>", false, prog, true);
-			} catch (e) {
-				alert(e)
-			}
-		});
-	}
+    runCode: function() {
+        Sk.main_canvas = $("canvas")[0];
+
+        //WIP: load an external script using AJAX
+        const scripturl = $("#program").attr("src");
+        if (scripturl) {
+            $.get(scripturl, function(data) {
+                //console.log(data);
+                app.exec(data);
+            });
+        } else {
+            app.exec($("#program").text());
+        }
+    },
+
+    exec: function(prog) {
+        Sk.misceval.asyncToPromise(function() {
+            try {
+                return Sk.importMainWithBody("<stdin>", false, prog, true);
+            } catch (e) {
+                alert(e)
+            }
+        });
+    }
 };
 
 $(app.init);
--- py/snake.py
+++ py/snake.py
@@ -1,31 +1,12 @@
 import pygame as pg
 from random import randint
-
-pg.init()
-print("Pygame initialized.");
-side = 40
-w = 32
-h = 18
-disp = pg.display.set_mode((w * side, h * side))
-
-game_running = 0
-
-dirs = [pg.K_RIGHT, pg.K_DOWN, pg.K_LEFT, pg.K_UP]
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
-snake = [(7, 3), (7, 4)]
-direction = 0
-
+import skulptcookies as cookies
 
 def place_apple():
     a = (randint(0, w - 1), randint(0, h - 1))
     while a in snake:
         a = (randint(0, w - 1), randint(0, h - 1))
     return a
-
-
-apple = place_apple()
-
 
 def check_collisions():
     head = snake[-1]
@@ -40,46 +21,98 @@ def check_collisions():
         return 1
     return 0
 
+def display_text(text, font, colour, pos):
+    ttl = font.render(text, False, colour)
+    (x,y) = pos
+    disp.blit(ttl, (x-ttl.get_width()/2, y-ttl.get_height()/2))
+
+def start_game():
+    global gamestate, score, snake, direction, apple
+    gamestate = GS_RUN
+    score = 0
+    snake = [(7, 3), (7, 4)]
+    direction = 0
+    apple = place_apple()
+    
+pg.init()
+print("Pygame initialized. Skulptcookies version: "+cookies.ver)
+
+GS_INTRO    = 0
+GS_RUN      = 1
+GS_GAMEOVER = 2
+GS_IDLE     = 3
+COLOR_WHITE = pg.Color("white")
+COLOR_RED = pg.Color("red")
+COLOR_GREEN = pg.Color("green")
+COLOR_BLUE = pg.Color("blue")
+
+highscore = cookies.get("score")
+if (highscore == None):
+    cookies.set("score",0)
+    highscore = 0
+else:
+    highscore = int(highscore)
+
+side = 40
+w = 32
+h = 18
+disp = pg.display.set_mode((w * side, h * side))
+
+dirs = [pg.K_RIGHT, pg.K_DOWN, pg.K_LEFT, pg.K_UP]
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
 
 clock = pg.time.Clock()
-f = pg.font.SysFont('console', 24, True)
+title_font = pg.font.SysFont('monospace', 36, True)
+text_font = pg.font.SysFont('monospace', 24, True)
+
+gamestate = GS_INTRO
 
 while True:
     clock.tick(8)
     for e in pg.event.get():
-        if e.type == pg.QUIT:
-            pg.quit()
-            break
         if e.type == pg.KEYDOWN:
-            if game_running == 0:
-                game_runnig = 1
             if e.key == pg.K_SPACE:
-                game_running = 0
-                snake = [(7, 3), (7, 4)]
-                direction = 0
-                apple = place_apple()
-            for i in range(4):
-                if e.key == dirs[i] and direction != (i + 2) % 4:
-                    direction = i
-    if game_running == 2:
+                start_game()
+            else:
+                for i in range(4):
+                    if e.key == dirs[i] and direction != (i + 2) % 4:
+                        direction = i
+    if gamestate == GS_INTRO:
         disp.fill(pg.Color("black"))
-        disp.blit(f.render("Game over!", False, pg.Color("blue")), (3 * side, 3 * side))
+        display_text("S N A K E",title_font,COLOR_WHITE, (640,300))
+        display_text("High Score: "+str(highscore), text_font, COLOR_GREEN, (640, 420))
+        display_text("Press SPACE to play", text_font, COLOR_WHITE, (640, 600))
         pg.display.update()
-        game_running = 3
-    if game_running == 3:
+        gamestate = GS_IDLE
+    if gamestate == GS_GAMEOVER:
+        disp.fill(pg.Color("black"))
+        display_text("Game over!",title_font,COLOR_RED, (640,300))
+        display_text("Your Score: "+str(score), text_font, COLOR_WHITE, (640,360))
+        display_text("High Score: "+str(highscore), text_font, COLOR_GREEN, (640, 420))
+        display_text("Press SPACE to play again", text_font, COLOR_WHITE, (640, 600))
+        pg.display.update()
+        gamestate = GS_IDLE
+    if gamestate == GS_IDLE:
         continue
-    snake.append((snake[-1][0] + dx[direction], snake[-1][1] + dy[direction]))
-    coll = check_collisions()
-    if coll >= 2:
-        game_running = 2
-        pass
-    elif coll == 1:
-        apple = place_apple()
-        pass
-    elif coll == 0:
-        snake.pop(0)
-    disp.fill(pg.Color("black"))
-    for i in range(len(snake)):
-        pg.draw.rect(disp, pg.Color("green"), pg.Rect(snake[i][0] * side, snake[i][1] * side, side, side))
-    pg.draw.rect(disp, pg.Color("red"), pg.Rect(apple[0] * side, apple[1] * side, side, side))
-    pg.display.update()
+    if gamestate == GS_RUN:
+        snake.append((snake[-1][0] + dx[direction], snake[-1][1] + dy[direction]))
+        coll = check_collisions()
+        if coll >= 2:
+            gamestate = GS_GAMEOVER
+        elif coll == 1:
+            apple = place_apple()
+            score += 1
+            if score > highscore:
+                highscore = score
+                cookies.set("score",highscore)
+        elif coll == 0:
+            snake.pop(0)
+        disp.fill(pg.Color("black"))
+        for i in range(len(snake)):
+            pg.draw.rect(disp, pg.Color("green"), pg.Rect(snake[i][0] * side, snake[i][1] * side, side, side))
+        pg.draw.rect(disp, pg.Color("red"), pg.Rect(apple[0] * side, apple[1] * side, side, side))
+
+        display_text("Your Score: "+str(score)+" High Score: "+str(highscore), text_font, COLOR_WHITE, (640,30))
+        pg.display.update()
+    
